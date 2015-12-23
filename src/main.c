a18fcd27f02bafb9ea770866b00396dc6c3f7b9b
sv99 <sv99@inbox.ru>
2015-12-23 11:56:02 +0300

    add send repeat

--- src/main.c
+++ src/main.c
@@ -3,8 +3,10 @@
 #include <sys/fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #define PULSE_BUFFER_SIZE       256
+#define REPEAT_TIMEOUT_MSEC     100
 
 #if defined(__arm__) || defined(__TARGET_ARCH_ARM)
 /* v2r davinchi arm platform */
@@ -26,29 +28,38 @@ unsigned int  pulse_array[PULSE_BUFFER_SIZE];
 //        4796, 1584,
 //        0};
 
-void send_array (unsigned int * array) {
-        int devfile = -1;
+void msleep(int ms) {
+        struct timespec time;
+        time.tv_sec = ms / 1000;
+        time.tv_nsec = (ms % 1000) * (1000 * 1000);
+        nanosleep(&time,NULL);
+}
+
+void send_array (unsigned int * array, int repeat) {
         size_t req_sz = 0;
         //считаем длинну массива
-        printf( "send_array start\n" );
-
         for (int i = 0; array[i]!=0; i++) {
-                printf( "%i: %i\n", i, array[i] );
+                // printf( "%i: %i\n", i, array[i] );
                 req_sz++;
         }
         req_sz++;
+
         //пересчитываем длинну массива в байтах
         req_sz=req_sz*sizeof(unsigned int);
 
         //открвываем файл длязаписи
-        devfile = open(IRSEND_DEVICE, O_WRONLY);
+        int devfile = open(IRSEND_DEVICE, O_WRONLY);
         if(devfile < 0){
                 perror("open");
                 exit(-1);
         }
+
         //записываем
-        if((write(devfile, array, req_sz))!=req_sz){
-                printf( "Device file Write Error!\n" );
+        for (int i = 0; i < repeat; i++) {
+                if((write(devfile, array, req_sz))!=req_sz){
+                        printf( "Device file Write Error!\n" );
+                }
+                msleep(REPEAT_TIMEOUT_MSEC);
         }
         close(devfile);
 }
@@ -59,10 +70,10 @@ main(int argc, char *argv[]) {
         int first = 1;
         if ( argc < 2 ) {
                 printf( "usage: %s [-c count_repeat] pulses ..\n", argv[0] );
-                return 1;
+                exit(-1);
         }
         else {
-                if ( !strcmp(argv[1], "-c") ) {
+                if ( strcmp(argv[1], "-c") == 0 ) {
                         repeat = atoi(argv[2]);
                         first = 3;
                 }
@@ -70,10 +81,9 @@ main(int argc, char *argv[]) {
                 for (int i = first; i < argc; i++) {
                         pulse_array[pulse_pos++] = (unsigned)atoi(argv[i]);
                 }
-                printf( "repeate: %i first: %i len: %i\n", repeat, first, pulse_pos);
                 pulse_array[pulse_pos] = 0;
-                printf( "len: %i\n", pulse_pos);
-                send_array((unsigned int*)&pulse_array);
+                printf( "repeate: %i pulses: %i\n", repeat, pulse_pos);
+                send_array((unsigned int*)&pulse_array, repeat);
         }
 
         return 0;
